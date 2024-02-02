e611ed65c4efea43be0a2a7493f34224b469d296
william <williams_wandji@yahoo.fr>
2024-02-02 13:46:43 +0100

    responsive end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Left from "./Components/Left/Left";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Pages/Home/Home";
@@ -9,8 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 
 export default function Nav() {
+  const [view,setview] = useState(false)
+  const onclickNavbar=()=>{
+    setview(!view)
+  }
   return (
-    <div id="content-nav">
+  <>  <div id="content-nav">
       <div className="content">
         <div className="left">
             <img src="Union.svg" />
@@ -16,7 +20,25 @@ export default function Nav() {
           <a href="#about-me"><p><span>#</span>about-me</p></a>
           <a href="#contact-me"><p><span>#</span>contacts</p></a>
         </div>
+        <div className="navbar-mobile" onClick={onclickNavbar}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+        </div>
+    
       </div>
-    </div>
+      </div>
+      {view? 
+         <div className="navbar-contains-mobile" onClick={onclickNavbar}>
+          <div className="left">
+            <img src="Union.svg" />
+            <p> WANDJI Williams</p>
+          </div>
+          <a href=""><p><span>#</span>home</p></a>
+          <a href="#project"><p><span>#</span>project</p></a>
+          <a href="#skills"><p><span>#</span>skills</p></a>
+          <a href="#about-me"><p><span>#</span>about-me</p></a>
+          <a href="#contact-me"><p><span>#</span>contacts</p></a>
+        </div>:<></>}
+    </>
   );
 }
