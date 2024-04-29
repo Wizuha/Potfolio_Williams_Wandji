75d26552ebc5e19a8cdef5d22bd547cd84e19177
william <williams_wandji@yahoo.fr>
2024-04-29 16:43:21 +0200

    xorrection nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,43 +2,88 @@ import React, { useState } from "react";
 import "./Nav.css";
 
 export default function Nav() {
-  const [view,setview] = useState(false)
-  const onclickNavbar=()=>{
-    setview(!view)
-  }
+  const [view, setview] = useState(false);
+  const onclickNavbar = () => {
+    setview(!view);
+  };
   return (
-  <>  <div id="content-nav">
-      <div className="content">
-        <div className="left">
-            <img src="Union.svg" />
-            <p> WANDJI Williams</p>
-        </div>
-        <div className="right">
-          <a href=""><p><span>#</span>home</p></a>
-          <a href="#project"><p><span>#</span>project</p></a>
-          <a href="#skills"><p><span>#</span>skills</p></a>
-          <a href="#about-me"><p><span>#</span>about-me</p></a>
-          <a href="#contact-me"><p><span>#</span>contacts</p></a>
-        </div>
-        <div className="navbar-mobile" onClick={onclickNavbar}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-        </div>
-    
-      </div>
-      </div>
-      {view? 
-         <div className="navbar-contains-mobile" onClick={onclickNavbar}>
+    <>
+      {" "}
+      <div id="content-nav">
+        <div className="content">
           <div className="left">
             <img src="Union.svg" />
             <p> WANDJI Williams</p>
           </div>
-          <a href=""><p><span>#</span>home</p></a>
-          <a href="#project"><p><span>#</span>project</p></a>
-          <a href="#skills"><p><span>#</span>skills</p></a>
-          <a href="#about-me"><p><span>#</span>about-me</p></a>
-          <a href="#contact-me"><p><span>#</span>contacts</p></a>
-        </div>:<></>}
+          <div className="right">
+            <a href="">
+              <p>
+                <span>#</span>home
+              </p>
+            </a>
+            <a href="#project">
+              <p>
+                <span>#</span>project
+              </p>
+            </a>
+            <a href="#skills">
+              <p>
+                <span>#</span>skills
+              </p>
+            </a>
+            <a href="#about-me">
+              <p>
+                <span>#</span>about-me
+              </p>
+            </a>
+            <a href="#contact-me">
+              <p>
+                <span>#</span>contacts
+              </p>
+            </a>
+          </div>
+          <div className="navbar-mobile" onClick={onclickNavbar}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+          </div>
+        </div>
+      </div>
+      {view ? (
+        <div className="navbar-contains-mobile">
+          <div className="left">
+            <img src="Union.svg" />
+            <p> WANDJI Williams</p>
+            <button onClick={onclickNavbar}>X</button>
+          </div>
+          <a href="">
+            <p>
+              <span>#</span>home
+            </p>
+          </a>
+          <a href="#project" onClick={onclickNavbar}>
+            <p>
+              <span>#</span>project
+            </p>
+          </a>
+          <a href="#skills" onClick={onclickNavbar}>
+            <p>
+              <span>#</span>skills
+            </p>
+          </a>
+          <a href="#about-me" onClick={onclickNavbar}>
+            <p>
+              <span>#</span>about-me
+            </p>
+          </a>
+          <a href="#contact-me" onClick={onclickNavbar}>
+            <p>
+              <span>#</span>contacts
+            </p>
+          </a>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
